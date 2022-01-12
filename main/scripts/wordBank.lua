c230d5fc69a69db00f3980259f2366dbb0816967
Blake Herrera <blake@blakeherrera.com>
2022-01-12 00:09:23 -0600

    highlight functionality progress (basic highlight complete, working on unhighlight)

---
+++ main/scripts/wordBank.lua
@@ -0,0 +1,13 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+local M = {} -- creates a new table in the local scope
+
+M.words = {"ナルト", "ルフィ", "ルルーシュヴィブリタニア", "ライト", "ボボボーボボーボボ", "アラジン", "ゴンフリークス", "コナン", "アレンウォーカー", "シロエ", "エドワードエルリック", "ピカチュウ"}
+-- 
+-- function M.setWords(newWords)
+-- 	M.words = newWords
+-- end
+
+return M
